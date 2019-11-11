93ce7692ec51b379efb6a8c2ba25236308ba3cd6
rkota <rosairekota@gmail.com>
2019-11-11 23:54:23 +0100

    'debut_des_users'

--- users_app/views.py
+++ users_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-	return render(request,"users/register.html",{})
+	form=UserCreationForm()
+	return render(request,"users/register.html",{"from":form})
